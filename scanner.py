ea60fa7e3c030669e833693d3f3d0b8289728d0a
parisa <parisa.payami@gmail.com>
2018-01-20 22:23:21 +0330

    scanner file and a test file added

--- scanner.py
+++ scanner.py
@@ -1,54 +1,81 @@
 import fileinput
 import re
 
-# create an array for reserved words
-
 reservedWords = ['EOF', 'public', 'class', '{', 'static', 'void', 'main', '()', '}', 'extends', ';', '(', ')',
                  'return', 'boolean', 'int', 'if', 'else', 'while', 'for', 'System.out.println', '.',
-                 'true', 'false', '&&', '==', '<', '-', '*', '+', '//', '/*', '*/']
+                 'true', 'false', '&&', '==', '<', '>', '-', '*', '+', '//', '/*', '*/', ',', '=']
 
 tokens = []
+
+symbolTable = []
+
+flag = False
 
 with open('test.txt') as test:
     lines = test.readlines()
 
-for line in lines:
+id = -1
 
+for line in lines:
     wordsInLine = line.split()
     for words in wordsInLine:
-        if words in reservedWords:
-            # dict = {words: reservedWords.index(words)}
-            dict = {words : 'reservedWord'}
-            tokens.append(dict)
 
-        elif len(words) == 1:
-            letterPattern = re.compile('[A-Za-z]')
-            digitPattern = re.compile('[0-9]')
-
-            if letterPattern.match(words):
-                dict = {words : 'letter'}
-                tokens.append(dict)
-            elif digitPattern.match(words):
-                dict = {words : 'digit'}
-                tokens.append(dict)
+        if flag == True:
+            if words == '*/':
+                flag = False
+                continue
             else:
-                print(words + "did not match any")
-
+                continue
 
         else:
-            identifierPattern = re.compile('[A-Za-z]([A-Za-z]|[0-9])+')
-            integerPattern = re.compile('[0-9]+')
+            if words in reservedWords:
+                if words == '//':
+                    break
 
-            if identifierPattern.match(words):
-                dict = {words : 'identifier'}
-                tokens.append(dict)
+                elif words == '/*':
+                    flag = True
+                    continue
 
-            elif integerPattern.match(words):
-                dict = {words : 'integer'}
-                tokens.append(dict)
+                else:
+                    dict = {words}
+                    tokens.append(dict)
+
+            elif len(words) == 1:
+                letterPattern = re.compile('[A-Za-z]')
+                digitPattern = re.compile('[0-9]')
+                id = id + 1
+
+                if letterPattern.match(words):
+                    dict = {'letter': id}
+                    tokens.append(dict)
+                    symbolTable.append(words)
+                elif digitPattern.match(words):
+                    dict = {'digit': id}
+                    tokens.append(dict)
+                    symbolTable.append(words)
+                else:
+                    print(words + "did not match any")
 
 
+            else:
+                id = id + 1
+                identifierPattern = re.compile('[A-Za-z]([A-Za-z]|[0-9])+')
+                integerPattern = re.compile('[0-9]+')
+
+                if identifierPattern.match(words):
+                    dict = {'identifier': id}
+                    tokens.append(dict)
+                    symbolTable.append(words)
+
+                elif integerPattern.match(words):
+                    dict = {'integer': id}
+                    tokens.append(dict)
+                    symbolTable.append(words)
 
 
-for token in tokens:
-    print(token)
+def sendNextToken():
+    data = [symbolTable, tokens[0]]
+
+    del tokens[0]
+
+    return data
